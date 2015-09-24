8f6ef22ee30717227aac344563344fc4b01073d5
untra <untra.sam@gmail.com>
2015-09-23 19:07:35 -0600

    wq

---
+++ publish.sh
@@ -0,0 +1,15 @@
+# Push source branch
+git checkout master
+git add -A
+git commit
+git push origin master
+
+# Push master branch
+jekyll
+git checkout master
+git rm -qr .
+cp -r _site/. .
+rm -r _site
+git add -A
+git commit
+git push origin master
