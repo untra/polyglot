14536715e42c6c3646b67d5abdd8cc2d26e13db1
untra <untra.sam@gmail.com>
2016-04-15 15:42:49 -0600

    updates to site

--- _plugins/polyglot.rb
+++ _plugins/polyglot.rb
@@ -1,4 +1,4 @@
-# Jekyll Polyglot v1.1.1
+# Jekyll Polyglot v1.2.0
 # Fast, painless, open source i18n plugin for Jekyll 3.0 Blogs.
 #   author: Samuel Volin (@untra)
 #   github: https://github.com/untra/polyglot
@@ -16,7 +16,7 @@ module Jekyll
       @file_langs = {}
       @default_lang = config['default_lang'] || 'en'
       @languages = config['languages'] || ['en']
-      @parallel_process_langs = config['parallel_localization'] || true
+      @parallel_localization = config['parallel_localization'] || true
       (@keep_files << @languages - [@default_lang]).flatten!
       @exclude_from_localization = config['exclude_from_localization'] || []
       @active_lang = @default_lang
@@ -25,8 +25,7 @@ module Jekyll
     alias_method :process_orig, :process
     def process
       prepare
-      # If use fork if possible, else fallback to synchronous processing (in windows)
-      if @parallel_process_langs
+      if @parallel_localization
         pids = {}
         languages.each do |lang|
           pids[lang] = fork do
@@ -45,7 +44,6 @@ module Jekyll
         end
       else
         languages.each do |lang|
-          puts "bad"
           process_language lang
         end
       end
@@ -73,84 +71,75 @@ module Jekyll
       process_orig
     end
 
-    # hook to coordinate blog posts into distinct urls,
-    # and remove duplicate multilanguage posts
-    Jekyll::Hooks.register :site, :post_read do |site|
+    # assigns natural permalinks to documents and prioritizes documents with
+    # active_lang languages over others
+    def coordinate_documents(docs)
+      regex = document_url_regex
       langs = {}
       approved = {}
-      n = ''
-      site.languages.each do |lang|
-        n += "([\/\.]#{lang}[\/\.])|"
-      end
-      n.chomp! '|'
-      site.posts.docs.each do |doc|
-        language = doc.data['lang'] || site.default_lang
-        url = doc.url.gsub(%r{#{n}}, '/')
+      docs.each do |doc|
+        language = doc.data['lang'] || @default_lang
+        url = doc.url.gsub(regex, '/')
         doc.data['permalink'] = url
-        next if langs[url] == site.active_lang
-        if langs[url] == site.default_lang
-          next if language != site.active_lang
+        next if langs[url] == @active_lang
+        if langs[url] == @default_lang
+          next if language != @active_lang
         end
         approved[url] = doc
         langs[url] = language
       end
-      site.posts.docs = approved.values
-    end
-  end
-
-  # Alteration to Jekyll Convertible module
-  # provides aliased methods to direct Convertible to skip files for write under
-  # certain conditions
-  module Convertible
-    def lang
-      data['lang'] || site.config['default_lang']
+      approved.values
     end
 
-    def lang=(str)
-      data['lang'] = str
-    end
-
-    alias_method :write_orig, :write
-    def write(dest)
-      path = polypath(dest)
-      return if skip?(path)
-      output_orig = output.clone
-      relativize_urls(site.active_lang)
-      write_orig(dest)
-      self.output = output_orig
-      site.file_langs[path] = lang
-    end
-
-    def polypath(dest)
-      n = ''
-      site.languages.each do |lang|
-        n += "(\\\.#{lang}\\/)|"
+    def process_documents(docs)
+      docs.each do |doc|
+        lang = doc.data['lang'] || @default_lang
+        path = doc.data['permalink']
+        relativize_urls doc
+        @file_langs[path] = lang
       end
-      n.chomp! '|'
-      destination(dest).gsub(%r{#{n}}, '/')
     end
 
-    def skip?(path)
-      return false if site.file_langs[path].nil?
-      return false if lang == site.active_lang
-      if lang == site.default_lang
-        return site.file_langs[path] == site.active_lang
+    # a regex that matches urls or permalinks with i18n prefixes or suffixes
+    # matches /en/foo , .en/foo , foo.en/ and other simmilar default urls
+    # made by jekyll when parsing documents without explicitly set permalinks
+    def document_url_regex
+      regex = ''
+      @languages.each do |lang|
+        regex += "([\/\.]#{lang}[\/\.])|"
       end
-      true
+      regex.chomp! '|'
+      %r{#{regex}}
     end
 
-    def relativize_urls(lang)
-      return if lang == site.default_lang
-      output.gsub!(relative_url_regex, "href=\"#{site.baseurl}/#{lang}/" + '\1"')
-    end
-
+    # a regex that matches relative urls in a html document
+    # matches href="baseurl/foo/bar-baz" and others like it
     def relative_url_regex
-      n = ''
-      site.exclude.each do |x|
-        n += "(?!#{x}\/)"
+      regex = ''
+      @exclude.each do |x|
+        regex += "(?!#{x}\/)"
       end
       # regex that looks for all relative urls except for excluded files
-      %r{href=\"#{site.baseurl}\/((?:#{n}[^,'\"\s\/?\.#-]+\.?)*(?:\/[^\]\[\)\(\"\'\s]*)?)\"}
+      %r{href=\"#{@baseurl}\/((?:#{regex}[^,'\"\s\/?\.#-]+\.?)*(?:\/[^\]\[\)\(\"\'\s]*)?)\"}
+    end
+
+    def relativize_urls(doc)
+      return if @active_lang == @default_lang
+      doc.output.gsub!(relative_url_regex, "href=\"#{@baseurl}/#{@active_lang}/" + '\1"')
+    end
+
+    # hook to coordinate blog posts and pages into distinct urls,
+    # and remove duplicate multilanguage posts and pages
+    Jekyll::Hooks.register :site, :post_read do |site|
+      site.posts.docs = site.coordinate_documents(site.posts.docs)
+      site.pages = site.coordinate_documents(site.pages)
+    end
+
+    # hook to coordinate blog posts and pages into distinct urls,
+    # and remove duplicate multilanguage posts and pages
+    Jekyll::Hooks.register :site, :post_render do |site|
+      site.process_documents(site.posts.docs)
+      site.process_documents(site.pages)
     end
   end
 
