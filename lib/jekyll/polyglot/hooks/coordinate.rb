fca88e823e6f866fefa8fd61d031f7d6ba8da535
Vladimir Sitnikov <sitnikov.vladimir@gmail.com>
2016-12-21 03:33:22 +0400

    Translate all the collections, not just "posts" (#45)

--- lib/jekyll/polyglot/hooks/coordinate.rb
+++ lib/jekyll/polyglot/hooks/coordinate.rb
@@ -1,6 +1,8 @@
 # hook to coordinate blog posts and pages into distinct urls,
 # and remove duplicate multilanguage posts and pages
 Jekyll::Hooks.register :site, :post_read do |site|
-  site.posts.docs = site.coordinate_documents(site.posts.docs)
+  site.collections.each do |_, collection|
+    collection.docs = site.coordinate_documents(collection.docs)
+  end
   site.pages = site.coordinate_documents(site.pages)
 end
--- lib/jekyll/polyglot/hooks/process.rb
+++ lib/jekyll/polyglot/hooks/process.rb
@@ -1,5 +1,7 @@
 # hook to make a call to process rendered documents,
 Jekyll::Hooks.register :site, :post_render do |site|
-  site.process_documents(site.posts.docs)
+  site.collections.each do |_, collection|
+    site.process_documents(collection.docs)
+  end
   site.process_documents(site.pages)
 end
