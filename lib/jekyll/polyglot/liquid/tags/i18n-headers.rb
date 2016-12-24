a4bb30fc0f44623b44009ca45cd20262fc6260bf
sam.volin <sam.volin@logrhythm.com>
2016-12-24 12:28:52 -0700

    updated I18n_Headers tag to default to the site.url when not provided

--- lib/jekyll/polyglot/liquid/tags/i18n-headers.rb
+++ lib/jekyll/polyglot/liquid/tags/i18n-headers.rb
@@ -12,13 +12,14 @@ module Jekyll
         def render(context)
           site = context.registers[:site]
           permalink = context.registers[:page]['permalink']
+          siteUrl = (@url.empty?) ? site.config['url'] : @url
           i18n = "<meta http-equiv=\"Content-Language\" content=\"#{site.active_lang}\">"
           i18n += "<link rel=\"alternate\" i18n=\"#{site.default_lang}\""\
-          " href=\"#{@url}#{permalink}\" />\n"
+          " href=\"#{siteUrl}#{permalink}\" />\n"
           site.languages.each do |lang|
             next if lang == site.default_lang
             i18n += "<link rel=\"alternate\" i18n=\"#{lang}\""\
-            " href=\"#{@url}/#{lang}#{permalink}\" />\n"
+            " href=\"#{siteUrl}/#{lang}#{permalink}\" />\n"
           end
           i18n
         end
