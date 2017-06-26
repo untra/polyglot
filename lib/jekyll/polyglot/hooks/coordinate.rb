675be4c539c1ca70ed4c0bfaf33834819567d06e
Peter Serwylo <peter@serwylo.com>
2017-06-26 10:47:04 +1000

    Recursively merge language specific `site.data`.
    Previously this would only overwrite the entire data files contents with
    that of a language specific version. Now it merges the language specific
    data file with the default, overriding whenever a new value is found in
    the language specific version. This ensures that untranslated strings
    revert back to their default value if a data file is only partially
    translated.

--- lib/jekyll/polyglot/hooks/coordinate.rb
+++ lib/jekyll/polyglot/hooks/coordinate.rb
@@ -5,9 +5,17 @@ Jekyll::Hooks.register :site, :post_read do |site|
 end
 
 def hook_coordinate(site)
-  if site.data.include?(site.active_lang)
-    site.data = site.data.merge(site.data[site.active_lang])
+  # Copy the language specific data, by recursively merging it with the default data.
+  # Favour active_lang first, then default_lang, then any non-language-specific data.
+  # See: https://www.ruby-forum.com/topic/142809
+  merger = proc { |key, v1, v2| Hash === v1 && Hash === v2 ? v1.merge(v2, &merger) : v2 }
+  if site.data.include?(site.default_lang)
+    site.data = site.data.merge(site.data[site.default_lang], &merger)
   end
+  if site.data.include?(site.active_lang)
+    site.data = site.data.merge(site.data[site.active_lang], &merger)
+  end
+
   site.collections.each do |_, collection|
     collection.docs = site.coordinate_documents(collection.docs)
   end
