0a32d134222342e907267549d122beb3627ae404
Peter Serwylo <peter@serwylo.com>
2017-06-28 08:34:36 +1000

    Added test coverage for merging language-specific and default site data
    Supports first prefering language specific strings for the active_lang, then falling back to those from default_lang (if they exist). Finally, if neither exist, it will try to fall back to the non-language specific default strings.

--- spec/tests/lib/jekyll/polyglot/hooks/coordinate_spec.rb
+++ spec/tests/lib/jekyll/polyglot/hooks/coordinate_spec.rb
@@ -20,7 +20,9 @@ describe 'hook_coordinate' do
       )
     )
     @site.prepare
-    @site.data = { 'foo' => 'databar', 'baz' => 'databaz' }
+    @site.data = { 'foo' => 'databar', 'baz' => 'databaz', 'strings' => {
+      'banana' => 'banana',
+    }, }
     @site.data['en'] = { 'foo' => 'enbar', 'strings' => {
       'apple' => 'apple', 'ice cream' => 'ice cream',
     }, }
@@ -33,5 +35,18 @@ describe 'hook_coordinate' do
     hook_coordinate(@site)
     expect(@site.data['foo']).to eq('enbar')
     expect(@site.data['baz']).to eq('databaz')
+    expect(@site.data['strings']['ice cream']).to eq('ice cream')
+    expect(@site.data['strings']['apple']).to eq('apple')
+    expect(@site.data['strings']['banana']).to eq('banana')
+  end
+
+  it 'should fall back to the default_lang when using translated site data' do
+    @site.active_lang = 'fr'
+    hook_coordinate(@site)
+    expect(@site.data['foo']).to eq('frbar')
+    expect(@site.data['baz']).to eq('databaz')
+    expect(@site.data['strings']['ice cream']).to eq('crème glacée') # Populated from @site.data['strings'][@site.active_lang]['ice cream']
+    expect(@site.data['strings']['apple']).to eq('apple') # Populated from @site.data['strings'][@site.default_lang]['apple']
+    expect(@site.data['strings']['banana']).to eq('banana') # Populated from @site.data['strings']['apple']
   end
 end
