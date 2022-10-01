82105aaa267ced23ea1161b90a2d72e11d15ba19
Samuel Volin <sam.volin@stackhawk.com>
2022-10-01 09:15:24 -0600

    Feature/polyglot 1.5.1 (#165)
    * polyglot 1.5.1
    
    * update build deps, add hebrew, polyglot 1.5.1

--- make.sh
+++ make.sh
@@ -1,8 +1,8 @@
 #! /bin/sh
-VERSION="1.5.0"
+VERSION="1.5.1"
 # this is running tests
 gem build jekyll-polyglot.gemspec
 gem install jekyll-polyglot-$VERSION.gem
 cd site
 rm -rf _site/
-jekyll build --no-watch
+bundle exec jekyll build --no-watch
