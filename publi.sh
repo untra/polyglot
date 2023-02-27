a34b69118c637f2e643b9a2f99fadcfcd5159aa8
untra <sam.volin@stackhawk.com>
2023-02-27 07:45:31 -0700

    Mon Feb 27 07:45:31 MST 2023

--- publi.sh
+++ publi.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 # change the branch names appropriately
 rm -rf site/_site/
-cd site && jekyll build && cd ..
+cd site && bundle exec jekyll build && cd ..
 git add site/_site/ && git commit -m "$(date)"
 git subtree push --prefix site/_site origin gh-pages
