013d45f7cc5c2c50e1b9bd58a5119342c5a86ece
Antonio Vázquez Blanco <antonio.vazquez@tarlogic.com>
2023-10-08 16:06:40 +0200

    Add a batch build script

---
+++ make.bat
@@ -0,0 +1,8 @@
+@echo off
+set VERSION="1.6.0"
+REM this is running tests
+CALL gem build jekyll-polyglot.gemspec
+CALL gem install jekyll-polyglot-%VERSION%.gem
+cd site
+rm -recurse -force _site/
+CALL bundle exec jekyll build --no-watch
