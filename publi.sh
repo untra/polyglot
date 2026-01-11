3b6753a5c6fdefbdc0566a5bb0e3638c9552c19d
untra <untra.sam@gmail.com>
2026-01-11 10:02:42 -0700

    update the gitignore, publi.sh to ignore built dir, just publi.sh to github pages, remove site/_site files

--- publi.sh
+++ publi.sh
@@ -2,6 +2,6 @@
 # change the branch names appropriately
 rm -rf site/_site/
 cd site && bundle exec jekyll build && cd ..
-git add site/_site/ && git commit -m "$(date)"
+git add -f site/_site/ && git commit -m "$(date)"
 git subtree push --prefix site/_site origin gh-pages
 
